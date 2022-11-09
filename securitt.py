031f75800858ad422caba3657cce1026970e9bdc
Tediore <49540618+Tediore@users.noreply.github.com>
2022-11-08 21:02:11 -0600

    enable hot reloading of alarm config options

--- securitt.py
+++ securitt.py
@@ -11,6 +11,41 @@ class Alarm:
     def __init__(self):
         self.modes = {'disarm': 'disarmed', 'arm_day_zones': 'armed_home', 'arm_night_zones': 'armed_night', 'arm_all_zones': 'armed_away'}
         self.ha_commands = {'disarm': 'disarm', 'arm_home': 'arm_day_zones', 'arm_night': 'arm_night_zones', 'arm_away': 'arm_all_zones'}
+        self.sensors = {}
+        self.sensor_list = []
+
+    def load_config(self, reload=False):
+        with open('/app/data/config.yaml', 'r') as config_file:
+            config = yaml.safe_load(config_file)
+            mqtt = config['mqtt']
+            sensors = config['sensors']
+            self.panel_settings = config['panel']
+            self.codes = self.panel_settings['codes']
+            self.keypads = config['keypads']
+            self.sirens = config['sirens']
+            self.z2m_topic = mqtt['z2m_topic']
+            self.log_settings = config['logging']
+        config_file.close()
+
+        for sensor in sensors:
+            name = sensor['name']
+            type = sensor['type']
+            instant = sensor['instant'] if 'instant' in sensor else False
+            active = sensor['active']
+            self.sensors[name] = {}
+            self.sensors[name]['type'] = type
+            self.sensors[name]['active'] = active
+            self.sensors[name]['instant'] = instant
+            self.sensor_list.append(name)
+
+        if not reload:
+            self.mqtt_host = mqtt['host'] if 'host' in mqtt else None
+            self.mqtt_port = mqtt['port'] if 'port' in mqtt else 1883
+            self.mqtt_user = mqtt['user'] if 'user' in mqtt else None
+            self.mqtt_pass = mqtt['password'] if 'password' in mqtt else None
+            self.mqtt_qos = mqtt['qos'] if 'qos' in mqtt else 1
+            self.base_topic = mqtt['base_topic'] if 'base_topic' in mqtt else 'securitty'
+            self.log_level = self.log_settings['log_level'].upper() if 'log_level' in self.log_settings else 'INFO'
 
     def keypad_input(self, action, keypad, code):
         """ Process input from alarm keypads """
@@ -54,7 +89,6 @@ class Alarm:
                 self.entry_delay(alarm_state)
 
     def set_mode(self, action, code, keypad=None):
-        """ Change alarm mode """
         code_list = self.codes
         user = code_list[int(code)]
         alarm_state = self.alarm_state
@@ -66,7 +100,7 @@ class Alarm:
             if alarm_state == 'triggered':
                 # if alarm is disarmed after being triggered
                 for siren in self.sirens:
-                    client.publish(f'{z2m_topic}/{siren}/set', json.dumps({"warning": {"mode": "stop", "strobe": "false", "duration": 1}}))
+                    client.publish(f'{self.z2m_topic}/{siren}/set', json.dumps({"warning": {"mode": "stop", "strobe": "false", "duration": 1}}))
                 alarm_timer = self.alarm_timer
                 alarm_timer.cancel()
 
@@ -87,10 +121,9 @@ class Alarm:
         if action != 'arm_all_zones':
             for pad in self.keypads:
                 # change keypad LEDs on alarm mode change
-                client.publish(f'{z2m_topic}/{pad}/set', json.dumps({'arm_mode': {'mode': action}}))
+                client.publish(f'{self.z2m_topic}/{pad}/set', json.dumps({'arm_mode': {'mode': action}}))
 
     def exit_delay(self, action, keypad, user):
-        """ Start the appropriate exit delay when alarm is armed """
         logger.info(f'Exit delay started by {user}')
         mode = self.modes[action]
         exit_delay = self.panel_settings[mode]['exit_delay']
@@ -105,14 +138,13 @@ class Alarm:
         }
 
         if keypad != None and action == 'arm_all_zones':
-            client.publish(f'{z2m_topic}/{keypad}/set', json.dumps({'arm_mode': {'mode': 'arming_away'}}))
+            client.publish(f'{self.z2m_topic}/{keypad}/set', json.dumps({'arm_mode': {'mode': 'arming_away'}}))
 
         self.exit_delay_timer = timers[mode]
         exit_delay_timer = self.exit_delay_timer
         exit_delay_timer.start()
 
     def entry_delay(self, state):
-        """ Start the appropriate entry delay when a monitored sensor is tripped """
         logger.debug(f'{self.trigger_sensor} tripped')
 
         self.prev_alarm_state = self.alarm_state
@@ -127,38 +159,32 @@ class Alarm:
         logger.info('Entry delay started')
 
     def alarm_mode_changed(self, user):
-        """ Save current and previous alarm mode to file """
         self.save_alarm_state()
         logger.info(f'Alarm state changed to {self.alarm_state} by {user}')
 
     def alarm_arm_away(self, user):
-        """ Arm alarm in away mode """
         self.prev_alarm_state = self.alarm_state
         self.alarm_state = 'armed_away'
         for pad in self.keypads:
-            client.publish(f'{z2m_topic}/{pad}/set', json.dumps({'arm_mode': {'mode': 'arm_all_zones'}}))
+            client.publish(f'{self.z2m_topic}/{pad}/set', json.dumps({'arm_mode': {'mode': 'arm_all_zones'}}))
         self.alarm_mode_changed(user)
 
     def alarm_arm_home(self, user):
-        """ Arm alarm in home mode """
         self.prev_alarm_state = self.alarm_state
         self.alarm_state = 'armed_home'
         self.alarm_mode_changed(user)
 
     def alarm_arm_night(self, user):
-        """ Arm alarm in night mode """
         self.prev_alarm_state = self.alarm_state
         self.alarm_state = 'armed_night'
         self.alarm_mode_changed(user)
 
     def alarm_disarmed(self, user):
-        """ Disarm alarm """
         self.prev_alarm_state = self.alarm_state
         self.alarm_state = 'disarmed'
         self.alarm_mode_changed(user)
 
     def alarm_triggered(self, state):
-        """ Sound the alarm(s) when the alarm is triggered """
         logger.info(f'Alarm triggered by {self.trigger_sensor}!')
 
         self.alarm_state = 'triggered'
@@ -170,7 +196,7 @@ class Alarm:
         alarm_timer.start()
 
         for siren in self.sirens:
-            client.publish(f'{z2m_topic}/{siren}/set', json.dumps({"warning": {"mode": "emergency", "strobe": "false", "duration": f'{siren_time}'}}))
+            client.publish(f'{self.z2m_topic}/{siren}/set', json.dumps({"warning": {"mode": "emergency", "strobe": "false", "duration": f'{siren_time}'}}))
 
     def restore_state_after_triggered(self):
         """ Return alarm to state before alarm was triggered """
@@ -183,7 +209,7 @@ class Alarm:
         """ Publish alarm state to MQTT and save state to file """
         state = self.alarm_state
         prev_state = self.prev_alarm_state
-        client.publish(f'{BASE_TOPIC}/alarm_state', state, retain=True)
+        client.publish(f'{self.base_topic}/alarm_state', state, retain=True)
 
         try:
             with open('/app/data/.state', 'w') as file:
@@ -195,12 +221,12 @@ class Alarm:
 def mqtt_connect():
     """Connect to MQTT broker and set LWT"""
     try:
-        client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
-        client.will_set(f'{BASE_TOPIC}/status', 'offline', 1, True)
+        client.username_pw_set(a.mqtt_user, a.mqtt_pass)
+        client.will_set(f'{a.base_topic}/status', 'offline', 1, True)
         client.on_connect = on_connect
         client.on_message = on_message
-        client.connect(MQTT_HOST, MQTT_PORT)
-        client.publish(f'{BASE_TOPIC}/status', 'online', 1, True)
+        client.connect(a.mqtt_host, a.mqtt_port)
+        client.publish(f'{a.base_topic}/status', 'online', 1, True)
     except Exception as e:
         logger.error(f'Unable to connect to MQTT broker: {e}')
         sys.exit(1)
@@ -211,23 +237,24 @@ def on_connect(client, userdata, flags, rc):
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
 
-    # topic for receiving commands from Home Assistant
-    client.subscribe(f'{BASE_TOPIC}/set_mode')
+    # topic for receiving commands from external sources
+    client.subscribe(f'{a.base_topic}/set_mode')
+    client.subscribe(f'{a.base_topic}/reload_config')
 
     # topics for receiving data from keypads and monitored sensors
-    for sensor in sensor_list:
-        client.subscribe(f'{z2m_topic}/{sensor}')
+    for sensor in a.sensor_list:
+        client.subscribe(f'{a.z2m_topic}/{sensor}')
     for keypad in a.keypads:
-        client.subscribe(f'{z2m_topic}/{keypad}')
+        client.subscribe(f'{a.z2m_topic}/{keypad}')
 
 def on_message(client, userdata, msg):
     topic = str(msg.topic)
-    if z2m_topic in topic:
+    if a.z2m_topic in topic:
         payload = json.loads(msg.payload.decode('utf-8'))
-        device = topic.replace(f'{z2m_topic}/','')
+        device = topic.replace(f'{a.z2m_topic}/','')
     
         # if monitored sensor changes state
-        if device in sensor_list: 
+        if device in a.sensor_list: 
             a.sensor_state_change(device, payload)
         
         # if an action is carried out on the keypad
@@ -242,7 +269,7 @@ def on_message(client, userdata, msg):
                     logger.debug(f"Received command from keypad '{keypad}': {action}")
 
     # receive command from Home Assistant
-    elif 'set_mode':
+    elif 'set_mode' in topic:
         payload = json.loads(msg.payload.decode('utf-8'))
         action = payload['action']
         code = payload['code']
@@ -257,38 +284,26 @@ def on_message(client, userdata, msg):
                 else:
                     logger.warning(f'Received invalid external command: {action}')
 
+    elif 'reload_config' in topic:
+        try:
+            a.load_config(reload=True)
+            logger.info('Configuration reloaded')
+        except Exception as e:
+            logger.error(f'Unable to reload configuration: {e}')
+
 if __name__ == '__main__':
 
     a = Alarm()
-
-    with open('/app/data/config.yaml', 'r') as config_file:
-        config = yaml.safe_load(config_file)
-        mqtt = config['mqtt']
-        a.panel_settings = config['panel']
-        a.codes = a.panel_settings['codes']
-        sensors = config['sensors']
-        a.keypads = config['keypads']
-        a.sirens = config['sirens']
-        z2m_topic = mqtt['z2m_topic']
-        log_settings = config['logging']
-        gotify_key = config['notify']['gotify_key']
-
-    MQTT_HOST = mqtt['host'] if 'host' in mqtt else None
-    MQTT_PORT = mqtt['port'] if 'port' in mqtt else 1883
-    MQTT_USER = mqtt['user'] if 'user' in mqtt else None
-    MQTT_PASSWORD = mqtt['password'] if 'password' in mqtt else None
-    MQTT_QOS = mqtt['qos'] if 'qos' in mqtt else 1
-    BASE_TOPIC = mqtt['base_topic'] if 'base_topic' in mqtt else 'securitt'
-    LOG_LEVEL = log_settings['log_level'].upper() if 'log_level' in log_settings else 'INFO'
+    a.load_config()
 
     logger = logging.getLogger('log')
-    log_days = log_settings['retain_days']
+    log_days = a.log_settings['retain_days']
     handler = TimedRotatingFileHandler('/app/data/securitt.log', when="midnight", backupCount=log_days)
 
-    if LOG_LEVEL.lower() not in ['debug', 'info', 'warning', 'error']:
+    if a.log_level.lower() not in ['debug', 'info', 'warning', 'error']:
         logging.basicConfig(level='INFO', format='%(asctime)s %(levelname)s: %(message)s', handlers=(handler,))
     else:
-        logging.basicConfig(level=LOG_LEVEL, format='%(asctime)s %(levelname)s: %(message)s', handlers=(handler,))
+        logging.basicConfig(level=a.log_level, format='%(asctime)s %(levelname)s: %(message)s', handlers=(handler,))
 
     logger.info('===== Starting securitt =====')
 
@@ -309,21 +324,7 @@ if __name__ == '__main__':
     logging.debug(f'Current alarm state: {a.alarm_state}')
     logging.debug(f'Previous alarm state: {a.prev_alarm_state}')
 
-    a.sensors = {}
-    sensor_list = []
-
-    for sensor in sensors:
-        name = sensor['name']
-        type = sensor['type']
-        instant = sensor['instant'] if 'instant' in sensor else False
-        active = sensor['active']
-        a.sensors[name] = {}
-        a.sensors[name]['type'] = type
-        a.sensors[name]['active'] = active
-        a.sensors[name]['instant'] = instant
-        sensor_list.append(name)
-
-    client = mqtt_client.Client(BASE_TOPIC)
+    client = mqtt_client.Client(a.base_topic)
 
     mqtt_connect()
     client.loop_forever()
